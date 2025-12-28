18894c7bc5cc2116ed5a7698413eb9452f812dc2
Mike Hansen <mike@unstablestudios.com>
2025-12-27 16:03:50 -0800

    feat(ui): implement base tailwind styling and fonts (#126)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,33 +1,22 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
-
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
+      {/* Planned layout:
+          - Left floating sidebar: project tree
+          - Right floating sidebar: tools
+          - Main content area: editors, properties, etc.
+          - Top bar: menu, app toolbar
+          - Bottom bar: status, logs
+      */}
+      <div className="h-16 w-full border-b gap-4 border-gray-300 flex items-center px-4 shadow-sm bg-white dark:bg-gray-800 dark:border-gray-700">
+        {/* Top Bar */}
+        <div className="flex items-baseline gap-2">
+          <h1 className="text-center align-middle font-extrabold tracking-tight text-3xl">
+            Gridfinity Studio
+          </h1>
+          <p className="text-center align-middle text-sm font-mono text-gray-500">v1.1.0</p>
         </div>
       </div>
-      <Versions></Versions>
     </>
   )
 }
