8f3800a7fa49e2b05226e89cbea9d6308ef40fc5
Mike Hansen <mike@mkhnsn.com>
2025-12-27 17:24:30 -0800

    feat(ui): install svgr and normalize configs

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,5 @@
-function App(): React.JSX.Element {
+import Logo from './components/Logo'
+export default function App(): React.JSX.Element {
   return (
     <>
       {/* Planned layout:
@@ -9,16 +10,8 @@ function App(): React.JSX.Element {
           - Bottom bar: status, logs
       */}
       <div className="h-16 w-full border-b gap-4 border-gray-300 flex items-center px-4 shadow-sm bg-white dark:bg-gray-800 dark:border-gray-700">
-        {/* Top Bar */}
-        <div className="flex items-baseline gap-2">
-          <h1 className="text-center align-middle font-extrabold tracking-tight text-3xl">
-            Gridfinity Studio
-          </h1>
-          <p className="text-center align-middle text-sm font-mono text-gray-500">v1.1.0</p>
-        </div>
+        <Logo />
       </div>
     </>
   )
 }
-
-export default App
