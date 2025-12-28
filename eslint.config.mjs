f7d12062886175c7d31fdf239da1000db5f3556a
Mike Hansen <mike@unstablestudios.com>
2025-12-28 08:05:46 -0800

    feat: initialize R3F UI and viewport (#130)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import ReactThree from '@react-three/eslint-plugin'
+import { ReactThreeFiber } from '@react-three/fiber'
 
 export default defineConfig(
   { ignores: ['**/ui/**', '**/node_modules', '**/dist', '**/out', '.husky', '.actrc'] },
@@ -21,12 +23,15 @@ export default defineConfig(
     files: ['**/*.{ts,tsx}'],
     plugins: {
       'react-hooks': eslintPluginReactHooks,
-      'react-refresh': eslintPluginReactRefresh
+      'react-refresh': eslintPluginReactRefresh,
+      '@react-three-fiber': ReactThreeFiber,
+      '@react-three': ReactThree
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
       ...eslintPluginReactRefresh.configs.vite.rules,
-      '@typescript-eslint/explicit-function-return-type': 'off'
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'react/no-unknown-property': 'off'
     }
   },
   eslintConfigPrettier
--- src/renderer/src/components/Navbar.tsx
+++ src/renderer/src/components/Navbar.tsx
@@ -4,10 +4,7 @@ import { ModeToggle } from '@/components/ui/mode-toggle'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuShortcut,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 
@@ -16,38 +13,64 @@ export default function Navbar(): React.JSX.Element {
     <div className="h-16 w-full border-b gap-4 border-zinc-300 flex items-center justify-between px-4 shadow-sm bg-white dark:bg-zinc-800 dark:border-zinc-700">
       <Logo />
       <div className="flex items-center gap-2">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">Menu</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="start">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                Profile
-                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Billing
-                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Keyboard shortcuts
-                <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuItem>
-              Log out
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <FileMenu />
+        <EditMenu />
+        <HelpMenu />
         <ModeToggle />
       </div>
     </div>
+  )
+}
+
+function FileMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">File</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuItem>New Project</DropdownMenuItem>
+        <DropdownMenuItem>Open Project</DropdownMenuItem>
+        <DropdownMenuItem>Open Recent...</DropdownMenuItem>
+        <DropdownMenuItem>Save</DropdownMenuItem>
+        <DropdownMenuItem>Save As...</DropdownMenuItem>
+        <DropdownMenuItem>Import...</DropdownMenuItem>
+        <DropdownMenuItem>Export...</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+function EditMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Edit</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuItem>Undo</DropdownMenuItem>
+        <DropdownMenuItem>Redo</DropdownMenuItem>
+        <DropdownMenuItem>Cut</DropdownMenuItem>
+        <DropdownMenuItem>Copy</DropdownMenuItem>
+        <DropdownMenuItem>Paste</DropdownMenuItem>
+        <DropdownMenuItem>Select All</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+function HelpMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Help</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuItem>Documentation</DropdownMenuItem>
+        <DropdownMenuItem>Community Forums</DropdownMenuItem>
+        <DropdownMenuItem>Report a Bug</DropdownMenuItem>
+        <DropdownMenuItem>About</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
