abc1e1249596aa93a07001687a79963539c3751b
Mike Hansen <mike@mkhnsn.com>
2025-12-27 17:35:07 -0800

    feat(ui): move header UI to Navbar and add placeholder nav items

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,8 @@
-import Logo from './components/Logo'
+import Navbar from './components/Navbar'
 export default function App(): React.JSX.Element {
   return (
     <>
-      {/* Planned layout:
-          - Left floating sidebar: project tree
-          - Right floating sidebar: tools
-          - Main content area: editors, properties, etc.
-          - Top bar: menu, app toolbar
-          - Bottom bar: status, logs
-      */}
-      <div className="h-16 w-full border-b gap-4 border-gray-300 flex items-center px-4 shadow-sm bg-white dark:bg-gray-800 dark:border-gray-700">
-        <Logo />
-      </div>
+      <Navbar />
     </>
   )
 }
